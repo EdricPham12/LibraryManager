ebc13c54a9cc317000fd7ad89177abc5a411444b
EdricPham12 <hoangpham27032005@gmail.com>
2025-05-17 19:06:57 +0700

    fix DBConnect.java and test successfully

--- src/util/DBConnect.java
+++ src/util/DBConnect.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnect {
     private static final String URL = "jdbc:mysql://localhost:3306/library";
     private static final String USER = "root";
-    private static final String PASSWORD = ""; //pass MySQL
+    private static final String PASSWORD = "0905632186aA"; //pass MySQL
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
